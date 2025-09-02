b2469b957434cfc932ebf12cdb93be4e6dc92edd
AudeSportisse <45034373+AudeSportisse@users.noreply.github.com>
2025-09-02 23:59:55 +0200

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,1 @@
-install.packages(c('missMDA', 'VIM'), dependencies = TRUE)
+install.packages(c('VIM', 'mice', 'misaem', 'parameters', 'caret', 'e1071', 'xgboost', 'randomForest', 'missForest', 'missMDA', 'FactoMineR'), dependencies = TRUE)
